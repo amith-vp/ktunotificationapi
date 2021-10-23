b69805d59c37dc837b3352b6b79416ef57089b61
Amith V Purushothaman <66194856+amith-vp@users.noreply.github.com>
2021-10-24 00:48:46 +0530

    Response failure - CORS
    added Cross-Origin Resource Sharing

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ setInterval(() => {
   scrapper.fetch()
 }, 300000);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 
 app.get('/', (req, res) => {
